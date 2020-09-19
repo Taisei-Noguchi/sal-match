860861c1db92755b057e7eb0189a48422fb7a5b1
Taisei-Noguchi <tarchan0320@icloud.com>
2020-09-20 08:34:22 +0900

    ユーザー登録機能ビュー

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,26 @@
 class TweetsController < ApplicationController
+  # before_action :authenticate_user!, only: [:new]
   def index
     @tweets = Tweet.order("created_at DESC")
   end
+end
+
+def new
+  @tweets = Tweet.new
+end
+
+def create
+  @tweets = Tweet.create(tweet_params)
+  if @tweets.save
+    redirect_to root_path
+  else
+    render :new
+  end
+
+end
+
+private
+
+def tweet_params
+  params.require(:tweet).permit(:title, :prefectures_id, :place, :field_id, :category_id, :price, :detail, :image).merge(user_id: current_user.id)
 end
