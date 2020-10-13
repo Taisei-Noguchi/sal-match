632334e22a6b856cedda77ff63a325af8c81babb
Taisei-Noguchi <tarchan0320@icloud.com>
2020-10-13 16:32:55 +0900

    投稿項目修正

--- db/migrate/20200918161048_create_tweets.rb
+++ db/migrate/20200918161048_create_tweets.rb
@@ -3,10 +3,10 @@ class CreateTweets < ActiveRecord::Migration[6.0]
     create_table :tweets do |t|
       t.string :title, null: false
       t.string :place, null: false
+      t.string :address, null: false
       t.string :price, null: false
-      t.string :detail, null: false
+      t.string :item, null: false
       t.integer :category_id, null: false
-      t.integer :prefecture_id, null: false
       t.integer :field_id, null: false
       t.integer :level_id, null: false
       t.integer :month_id, null: false
